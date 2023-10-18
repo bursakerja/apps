070bba3d5c6afe8f39fdede3c08196996a0250bd
dimasardnt6 <dimasardnt6@gmail.com>
2023-10-18 19:03:02 +0700

    push

--- build/js/autentikasiToken.js
+++ build/js/autentikasiToken.js
@@ -2,12 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   // Periksa apakah ada token dalam cookie
   const token = getCookie("token");
 
+  // Jika tidak ada token, redirect ke halaman signIn.html
   if (!token) {
-    // Jika tidak ada token, redirect ke halaman signIn.html
     window.location.href = "signIn.html";
   }
-});
 
-export default function PostSignUp() {
-  // ... (kode login yang ada sebelumnya)
-}
+  // Fungsi untuk mendapatkan nilai cookie berdasarkan nama
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
+});
