e84ee62e219c6e225135e88579a61fd7c0c64927
dimasardnt6 <dimasardnt6@gmail.com>
2023-10-18 19:45:02 +0700

    push

--- build/js/postFunction.js
+++ build/js/postFunction.js
@@ -1,21 +1,5 @@
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
-// import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
-
-export default function PostSignUp() {
-  let target_url =
-    "https://asia-southeast2-bursakerja-project.cloudfunctions.net/bursakerja-post";
-  let tokenkey = "token";
-  let tokenvalue =
-    "3108501ddf2f9aa33f3a7c0e387339b131d8a4d22818feb511b0fe1bc3a16b36";
-  let datainjson = {
-    email: getValue("email"),
-    password: getValue("password"),
-  };
-
-  postWithToken(target_url, tokenkey, tokenvalue, datainjson, responseData);
-  //   alert("testing2");
-}
 
 function setCookieWithExpireHour(cname, cvalue, exhour) {
   const d = new Date();
@@ -28,6 +12,22 @@ function setCookieWithExpireHour(cname, cvalue, exhour) {
     ";" +
     expires +
     "Secure;HttpOnly;SameSite=Strict;path=/pelamar";
+}
+
+export default function PostSignUp() {
+  let target_url =
+    "https://asia-southeast2-bursakerja-project.cloudfunctions.net/bursakerja-post";
+  let tokenkey = "token";
+  let tokenvalue =
+    "3108501ddf2f9aa33f3a7c0e387339b131d8a4d22818feb511b0fe1bc3a16b36";
+  let datainjson = {
+    email: getValue("email"),
+    password: getValue("password"),
+  };
+
+  // Panggil fungsi postWithToken
+  postWithToken(target_url, tokenkey, tokenvalue, datainjson, responseData);
+  //   alert("testing2");
 }
 
 function responseData(result) {
