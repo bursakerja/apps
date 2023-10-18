b22550b572f508e88ceca9e02ab0183410c7b4ce
dimasardnt6 <dimasardnt6@gmail.com>
2023-10-18 15:28:12 +0700

    push

--- build/js/signIn.js
+++ build/js/signIn.js
@@ -2,6 +2,7 @@ function signIn() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
+  // Make a POST request to the sign-in endpoint
   fetch(
     "https://asia-southeast2-bursakerja-project.cloudfunctions.net/bursakerja-post",
     {
@@ -19,22 +20,16 @@ function signIn() {
     .then((data) => {
       if (data.status === 200) {
         // Signin success
-        // Save the token in cookies with an expiry time (in seconds)
-        // const expiryTime = 3600; // 1 hour (in seconds)
-        // const now = new Date();
-        // now.setTime(now.getTime() + expiryTime * 1000);
-        // document.cookie = `token=${
-        //   data.token
-        // }; expires=${now.toUTCString()}; path=/`;
+        // Save the token in local storage
         localStorage.setItem("token", data.token);
-
-        console.log(data);
 
         // Display success message using SweetAlert
         Swal.fire({
           icon: "success",
           title: "Sign In Successful",
-          text: data.message,
+          text: "You have successfully signed in.",
+          showConfirmButton: false,
+          timer: 2000,
         }).then(() => {
           // Redirect to homePelamar.html
           window.location.href = "homePelamar.html";
@@ -45,24 +40,16 @@ function signIn() {
           icon: "error",
           title: "Sign In Failed",
           text: data.message,
-        }).then(() => {
-          // Redirect back to signIn.html on failure
-          window.location.href = "signIn.html";
         });
       }
     })
     .catch((error) => {
-      console.error("Error:", error);
+      console.log("Error:", error);
       // Display error message using SweetAlert
       Swal.fire({
         icon: "error",
         title: "Error",
         text: "An error occurred while signing in. Please try again later.",
-      }).then(() => {
-        // Redirect back to signIn.html on error
-        window.location.href = "signIn.html";
       });
     });
 }
-
-console.log(data);
