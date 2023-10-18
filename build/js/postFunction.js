8369f10c4c9e74f4d91a984fd5a1f2804051af9a
dimasardnt6 <dimasardnt6@gmail.com>
2023-10-18 16:50:03 +0700

    push

--- build/js/postFunction.js
+++ build/js/postFunction.js
@@ -22,4 +22,6 @@ function responseData(result) {
   console.log("test:", result.message);
   setInner("pesan", result.message);
   setCookieWithExpireHour("token", result.token, 2);
+  // Redirect to homePelamar.html
+  window.location.href = "homePelamar.html";
 }
