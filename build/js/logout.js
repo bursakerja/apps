163fbec3a72361a699739b25d13631c5211c13aa
dimasardnt6 <dimasardnt6@gmail.com>
2023-10-19 16:36:05 +0700

    push

--- build/js/logout.js
+++ build/js/logout.js
@@ -9,11 +9,11 @@ function logout() {
     cancelButtonText: "Batal",
   }).then((result) => {
     if (result.isConfirmed) {
-      // Menghapus message dari local storage
-      localStorage.removeItem("message"); // Jika menggunakan local storage
       // Menghapus toke dari cookie
       document.cookie =
         "token= ; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      document.cookie =
+        "message= ; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
       // Redirect ke halaman sign-in.html
       window.location.href = "../signIn.html";
     }
