783eb8b0c83a9dd79f1cf6244ea53aaceea38c6e
dimasardnt6 <dimasardnt6@gmail.com>
2023-10-19 15:28:09 +0700

    push

---
+++ build/js/getCookie.js
@@ -0,0 +1,3 @@
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+document.getElementById("cookie").innerHTML = getCookie("token");
---
+++ build/js/logout.js
@@ -0,0 +1,23 @@
+import { deleteCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+// Fungsi untuk logout
+function logout() {
+  // Menampilkan Sweet Alert konfirmasi
+  Swal.fire({
+    icon: "question",
+    title: "Konfirmasi",
+    text: "Apakah Anda yakin ingin logout?",
+    showCancelButton: true,
+    confirmButtonText: "Logout",
+    cancelButtonText: "Batal",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // Menghapus message dari local storage
+      localStorage.removeItem("message"); // Jika menggunakan local storage
+      // Menghapus toke dari cookie
+      deleteCookie("token");
+      // Redirect ke halaman sign-in.html
+      window.location.href = "../signIn.html";
+    }
+  });
+}
